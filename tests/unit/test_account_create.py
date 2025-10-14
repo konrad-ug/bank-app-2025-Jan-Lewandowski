3523f441da970bd1a01f98ce8ab7fd3f43087b1c
Jan Lewandowski <jlewandowski@INF.UG.EDU.PL>
2025-10-14 15:20:21 +0200

    Added feature 2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,10 +3,11 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12312321312")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         account.balance = 100
         assert account.balance == 100
         account.balance += 100
         assert account.balance == 200
+        assert account.pesel == "12312321312"

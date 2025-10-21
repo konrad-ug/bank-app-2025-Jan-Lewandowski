84bb3c81fb193da430dd4f9aa744307d02cd5e19
Jan Lewandowski <jlewandowski@inf.ug.edu.pl>
2025-10-21 15:44:20 +0200

    Lab3 feature(6-7)

---
+++ src/company_account.py
@@ -0,0 +1,10 @@
+from src.account import Account
+
+class CompanyAccount(Account):
+    def __init__(self, company_name, nip):
+        self.company_name = company_name
+        self.nip = nip if self.is_nip_valid(nip) else "Niepoprawny NIP!"
+        self.balance = 0.0
+    
+    def is_nip_valid(self, pesel):
+        return (len(pesel) == 10 and pesel.isdigit())
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -1,6 +1,7 @@
 from src.account import Account
 
 
+
 class TestAccount:
     def test_account_creation(self):
         account = Account("John", "Doe", "123")
@@ -91,3 +92,70 @@ class TestAccount:
     def test_account_promo_suffix_too_short_old(self):
         account = Account("John", "Doe", "55123456789","PROM_a")
         assert account.balance == 0.0
+
+    def test_account_get_positive_amount(self):
+        account = Account("John", "Doe", "55123456789","PROM_a")
+        assert account.get(50.0) == 50.0
+        assert account.balance == 50.0
+
+    def test_account_get_negative_amount(self):
+        account = Account("John", "Doe", "55123456789","PROM_a")
+        assert account.get(-50.0) == 0.0
+        assert account.balance == 0.0   
+
+    def test_account_send_enough_money(self):
+        account = Account("John", "Doe", "55123456789","PROM_a")
+        account.balance = 50.0
+        account.send(30.0)
+        assert account.balance == 20.0
+    
+    def test_account_send_not_enough_money(self):
+        account = Account("John", "Doe", "55123456789","PROM_a")
+        account.balance = 0.0
+
+        assert account.send(30.0) == 0.0
+
+    def test_account_send_negative_amount(self):
+        account = Account("John", "Doe", "55123456789","PROM_a")
+        account.balance = 30.0
+        account.send(-20.0)
+        assert account.balance == 30.0
+
+
+class TestCompany:
+    def test_company_account_valid_pesel(self):
+        company_account = CompanyAccount("firma", "1231231231")
+        assert company_account.nip == "1231231231"
+    
+    def test_company_account_wrong_pesel(self):
+        company_account = CompanyAccount("firma", "1231213213231231abc")
+        assert company_account.nip == "Niepoprawny NIP!"
+
+    
+    def test_account_get_positive_amount(self):
+        company_account = CompanyAccount("firma", "1231231231")
+        assert company_account.get(50.0) == 50.0
+        assert company_account.balance == 50.0
+
+    def test_account_get_negative_amount(self):
+        company_account = CompanyAccount("firma", "1231231231")
+        assert company_account.get(-50.0) == 0.0
+        assert company_account.balance == 0.0   
+
+    def test_account_send_enough_money(self):
+        company_account = CompanyAccount("firma", "1231231231")
+        company_account.balance = 50.0
+        company_account.send(30.0)
+        assert company_account.balance == 20.0
+    
+    def test_account_send_not_enough_money(self):
+        company_account = CompanyAccount("firma", "1231231231")
+        company_account.balance = 0.0
+
+        assert company_account.send(30.0) == 0.0
+
+    def test_account_send_negative_amount(self):
+        company_account = CompanyAccount("firma", "1231231231")
+        company_account.balance = 30.0
+        company_account.send(-20.0)
+        assert company_account.balance == 30.0

a98e18438baf1def70b5102dfbcd78c0b1142c78
jan_lewandowski010 <JanekiLewandowski@outlook.com>
2025-11-25 15:56:56 +0100

    lab7 do s

---
+++ app/api.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from src.account_registry import AccountRegistry
+from src.personal_account import PersonalAccount
+
+app = Flask(__name__)
+registry = AccountRegistry()
+
+@app.route("/api/accounts", methods=['POST'])
+def create_account():
+    data = request.get_json()
+    print(f"Create account request: {data}")
+    account = PersonalAccount(data["name"], data["surname"], data["pesel"])
+    registry.add_account(account)
+    return jsonify({"message": "Account created"}), 201
+
+@app.route("/api/accounts", methods=['GET'])
+def get_all_accounts():
+    print("Get all accounts request received")
+    accounts = registry.get_all_accounts()
+    accounts_data = [{"name": acc.first_name, "surname": acc.last_name, "pesel":acc.pesel, "balance": acc.balance} for acc in accounts]
+    
+    return jsonify(accounts_data), 200
+
+@app.route("/api/accounts/count", methods=['GET'])
+def get_account_count():
+    print("Get account count request received")
+    count = registry.get_account_count()
+    return jsonify({"count": count}), 200
+
+@app.route("/api/accounts/<pesel>", methods=['GET'])
+def get_account_by_pesel(pesel):
+    found_person = registry.search_account_based_on_pesel(pesel)
+    return jsonify(found_person), 200
+
+@app.route("/api/accounts/<pesel>", methods=['PATCH'])
+def update_account(pesel):
+    #implementacja powinna znaleźć się tutaj
+    return jsonify({"message": "Account updated"}), 200
+
+@app.route("/api/accounts/<pesel>", methods=['DELETE'])
+def delete_account(pesel):
+    #implementacja powinna znaleźć się tutaj
+    return jsonify({"message": "Account deleted"}), 200
